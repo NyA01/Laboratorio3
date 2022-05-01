2f9daccb75dabfebfd22c20f7f3f2a9886405439
Nayeri Azofeifa <NAYERI.AZOFEIFA@ucr.ac.cr>
2022-05-01 13:12:57 -0600

    Segunda parte del labotatorio: Song

---
+++ Laboratorio3/Laboratorio3/Controllers/SongController.cs
@@ -0,0 +1,34 @@
+﻿using Laboratorio3.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+
+namespace Laboratorio3.Controllers
+{
+    public class SongController : Controller
+    {
+        public IActionResult Index()
+        {
+            //var movies = GetListOfMovies();
+            var song = GetFavoriteSong();
+            ViewBag.MainTitle = "My favorite song";
+            return View(song);
+        }
+
+        private List<SongModel> GetFavoriteSong()
+        {
+            List<SongModel> song = new List<SongModel>();
+            song.Add(new SongModel
+            {
+                Name = "A Sky Full of Stars",
+                Autor = "Coldplay",
+                Genre = "pop-rock",
+                Album = "Ghost Stories",
+                ReleasedDate = new DateTime(2010, 05, 2)
+            });
+            return song;
+        }
+    }
+}
---
+++ Laboratorio3/Laboratorio3/Models/SongModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+
+namespace Laboratorio3.Models
+{
+    public class SongModel
+    {
+        public string Name { get; set; }
+        public string Autor { get; set; }
+        public string Genre { get; set; }
+        public string Album { get; set; }
+        public DateTime ReleasedDate { get; set; }
+    }
+}
